c18ddffe2ef38428eae38a6f87f489c91969730d
kyary <etoile.gale@ynov.com>
2018-11-05 15:05:44 +0100

    first commit project

---
+++ src/main/java/Business/Tour/BusinessTourApplication.java
@@ -0,0 +1,12 @@
+package Business.Tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessTourApplication.class, args);
+	}
+}
